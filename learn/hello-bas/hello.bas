616869090e8bc316b6191485c3f5fc3a8d1ad510
barrettotte <barrettotte@gmail.com>
2025-05-22 22:13:05 -0400

    adds BASIC example

---
+++ learn/hello-bas/hello.bas
@@ -0,0 +1,4 @@
+1  rem test
+2  rem note: use lowercase for petscii issues
+10 print "hello world"
+20 goto 10
--- run.cmd
+++ run.cmd
@@ -5,19 +5,22 @@
 SET "REPO_DIR=%~dp0"
 IF "%REPO_DIR:~-1%" == "\" SET "REPO_DIR=%REPO_DIR:~0,-1%"
 
-SET KICKASS="D:/programs/KickAssembler/kickass.jar"
+SET VICE_DIR="D:\\programs\\GTK3VICE-3.9-win64\\bin"
+SET "VICE_64SC=%VICE_DIR%\\x64sc.exe"
+SET "VICE_PETCAT=%VICE_DIR%\\petcat.exe"
+SET "VICE_CONFIG=%REPO_DIR%\\vice-config.ini"
 
-SET VICE_64SC="D:/programs/GTK3VICE-3.9-win64/bin/x64sc.exe"
-SET VICE_CONFIG="%REPO_DIR%/vice-config.ini"
-
-SET PRG="%REPO_DIR%/tea.prg"
+SET "PRG=%REPO_DIR%\\tea.prg"
 
 @REM Compile source to .pgm
-java -jar %KICKASS% "%REPO_DIR%/learn/hello-asm/hello.asm" -o %PRG% -showmem
+@ECHO Compiling...
+@REM java -jar %KICKASS% "%REPO_DIR%\\learn\\hello-asm\\hello.asm" -o %PRG% -showmem
+%VICE_PETCAT% -w2 -o %PRG% -- "%REPO_DIR%\\learn\\hello-bas\\hello.bas"
 
 IF %ERRORLEVEL% NEQ 0 ECHO Build failed & EXIT 1
 
 @REM Launch VICE with .pgm loaded
+@ECHO Launching emulator...
 %VICE_64SC% -config %VICE_CONFIG% %PRG%
 
 @REM SYS 49152
